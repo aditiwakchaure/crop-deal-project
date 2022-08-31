fe778b1cab8c845666a7c4dfa89f6b24f0e82405
aditiwakchaure <aditiwakchaure.31@gmail.com>
2022-08-31 16:13:14 +0530

    Added bill microservice

---
+++ bill-service1/src/main/java/com/cropdeal/billservice1/BillService1Application.java
@@ -0,0 +1,19 @@
+package com.cropdeal.billservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@SpringBootApplication
+
+@EnableEurekaClient
+@EnableSwagger2
+public class BillService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillService1Application.class, args);
+	}
+
+}
---
+++ bill-service1/src/main/java/com/cropdeal/billservice1/service/BillService.java
@@ -0,0 +1,28 @@
+package com.cropdeal.billservice1.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cropdeal.billservice1.entity.Bill;
+import com.cropdeal.billservice1.repository.BillRepository;
+import java.util.List;
+@Service
+public class BillService {
+	
+@Autowired
+private BillRepository repository;
+public Bill saveBill(Bill bill) {
+
+	return repository.save(bill);
+}
+//public List<user1> saveUsers(List<user1> users) {
+
+	//return repository.saveAll(users);
+//}
+public List<Bill> getAllBills(){
+	return repository.findAll();
+}
+public Bill getBillById(int billid){
+	return repository.findById(billid);
+}
+}
